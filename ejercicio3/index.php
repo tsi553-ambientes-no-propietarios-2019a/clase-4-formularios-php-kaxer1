bedca72c53978ae6063e40d2610da77f30f8ab83
kaxer1 <kevin.curay@epn.edu.ec>
2019-04-23 19:42:17 -0500

    ejercicio3

--- ejercicio3/index.php
+++ ejercicio3/index.php
@@ -1,14 +1,83 @@
-<?php
+﻿<?php
 /**
- * Crear un arreglo asociativo que almacene las claves de acceso de 5 usuarios de un sistema. Acceder a cada componente por su nombre.
- El arreglo debe tener el formato
- [
-    'juan' => 'contrasena_segura'
- ]
- Crear un formulario que permita ingresar el nombre de usuario y la contraseña. 
- Al enviar el formulario se debe validar que la combinación de nombre de usuario y contraseña existan en el arreglo.
- Si los datos no son correctos imprimir un mensaje de error.
- Si los datos son correctos debe redirigir a otra página bienvenido.php que debe imprimir el mensaje "Bienvendo JUAN". (con el nombre de usuario en mayúsculas)
+ * Crear un arreglo asociativo que almacene las claves de acceso de 5 usuarios de un sistema. 
+ * Acceder a cada componente por su nombre.
+ * 
+ * Crear un formulario que permita ingresar el nombre de usuario y la contraseña. Al enviar el 
+ * formulario se debe validar los datos contra el arreglo para imprimir un mensaje que muestre un 
+ * mensaje de error o que permita direccionar a otra página si los datos son correctos.
  */
 
+    $registro=array(
+            'Pedro'=>'12345',
+            'Luis'=>'lolo',
+            'Alejandra'=>'esponja',
+            'Katness'=>'paleta',
+            'Peeta'=>'sinsajo',
+            );
+
+    $email = $_POST['id_user'];
+    $password = $_POST['id_pass'];
+
+    if($_SERVER['REQUEST_METHOD'] == 'POST'){
+        foreach($registro as $keys => $valor){
+            if($keys == $email && $valor == $password){
+                header('Location: pagina.html');
+            }
+        }
+    }
+    
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Ejercicio 3</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+</head>
+<body>
+    <br>
+    <br>
+
+    <h1> <center> INICIAR SESIÓN </center> </h1>
+    <br>
+
+    <form action="index.php" method="POST">
+    <div class="container">
+        <div class="row">
+        <div class="col-sm">
+
+        </div>
+        <div class="col-sm-5">
+            <div class="form-group row">
+                <label for="inputUser" class="col-sm-2 col-form-label">User</label>
+            <div class="col-sm-10">
+                <input type="text" class="form-control" name="id_user" id="id_user" placeholder="User">
+            </div>
+            </div>
+            <div class="form-group row">
+                <label for="inputPassword" class="col-sm-2 col-form-label">Password</label>
+            <div class="col-sm-10">
+                <input type="password" class="form-control" name="id_pass" id="id_pass" placeholder="Password">
+            </div>
+            </div>
+                <input type="submit" class="btn btn-primary mb-2" name="ingresar" value="ingresar"  />
+        </div>
+        <div class="col-sm">
+            
+        </div>
+        </div>
+    </div>
+    </form>
+
+    
+    
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
+</body>
+</html>
